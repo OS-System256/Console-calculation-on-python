b68c0c5bcdf8d21f08e5cbb5325e6abeaa4c1cce
OS-System256 <ArtemLemans@gmail.com>
2024-12-06 08:00:28 +0300

    Написал код который работает не отличима от кода консольного калькулятора на java в коммите от 2024年11月21日 午前0:15 по МСК.Мое мнение про этот код и про python: В итоге код консольного калькулятора на Python занял столькоже строк кода, что на Java с одинаковой работой, если из кода на java убрать отступы (которые в отличии от python ничего не изменит и закрывающийся '{' переместить на строчку выше. Но на python как-то крива работает обЪекты для ООП, а именно я не смог сделать для каждого класса свой файл (например, как в java). У каждого языка программирование свои плюсы и минусы, каждый подходит для свои задач, но пока я еще новичек я не смог увидеть значительного различие в этих 2-ух языках программирование, а если чесно думаю и не увиже так как и java, и python C подомный языки (ну если не учитывать особеность python в том что пробелы являются значемыми и используются вместо '{'), хотя все говорят, что для новичков лучше всего учить java, но для меня проще java, чем python (возможно из-за того что я начел изучать java раньше, чем python и то что я особо не знал синтаксес python в отличии от java, хотя даже сейчас я считаю, что мне java как-то понятнее и проще, но при этом я понимаю, что отказываться от python я не собераюсь, как минимум из-за его распространение и популярности в других сверах в том числе для нейроного обучение, а так же для меня будет полезно так же python библиотеке по мимо некроного обучения, которые меня интересует, так же парсера, скриптинго (очень распространен для скриптов на линукс (на которой я пользуюсь), ну раньше так же плюсом огромное количество учебного материала из-за распроненасти языка программирования, которые могут помоч для обучения на java и сейчас я думаю так же, так же из-за распростроненасти языка программированея в разных областях, которые только коственно или вообще не связаны с программированием, таких как QA тестировщик (тестировщик, который автоматически (через скрипты и ПО) тестирует ПО), DevOps (специалист и по программирование, и по системному администрированию) и так далее(и как минимум меня интересует системное адменистрирование и программироване(а значит и DevOps))

--- Calculator_on_python.py
+++ Calculator_on_python.py
@@ -2,24 +2,28 @@ class User_interfaces:
     error: str
     one_number: float
     two_number: float
-    result: float
     answer: str
-    def start():
+    def start_user_interfaces():
         User_interfaces.answer = input("Ведите первое число: ")
         User_interfaces.one_number = float(User_interfaces.answer)
-        User_interfaces.Main_interfaces()
-    def Main_interfaces():
+        User_interfaces.main_user_interfaces()
+    def main_user_interfaces():
         User_interfaces.answer = input("Ведите второе число: ")
         User_interfaces.two_number = float(User_interfaces.answer)
         User_interfaces.answer = input("1. Вычитание\n2. Деление\n3. Нахождение остатка при делении\n4. Сложение\n5. Умножение\n:: Выбирете действие (1-5):\n:: ")
         Calculation.calculation()
         print("Результат: " + str(Calculation.result))
-        answer = input("Начать вычесление над результатом? [y/n] ")
-        if (answer == 'y' or answer == 'Y'):
-            print(True)
-    def Answer_error():
-        print("Ошибка: " + User_interfaces.error)
-        exit
+        User_interfaces.answer = input("Начать вычесление над результатом? [y/n] ")
+        if (User_interfaces.answer == 'y' or User_interfaces.answer == 'Y'):
+            User_interfaces.main_user_interfaces()
+        if (User_interfaces.answer == 'n' or User_interfaces.answer == 'N'):
+            exit
+        if (User_interfaces.answer != 'y' and User_interfaces.answer != 'Y' and User_interfaces.answer != 'n' and User_interfaces.answer != 'N'):
+            User_interfaces.error = "ответ не понятен (надо было написать y (что означает, что вы согласны) или n (вы не согласны))"
+            User_interfaces.answer_error()
+    def answer_error():
+        print("Ошибка: " + User_interfaces.error + ". Перезагрузка программы...")
+        User_interfaces.start_user_interfaces()
 class Calculation:
     result: float
     def calculation():
@@ -38,5 +42,7 @@ class Calculation:
             Calculation.result = one_number * two_number
         if (int_answer < 1 or int_answer > 5):
             User_interfaces.error = "данная операция не найдена"
-            User_interfaces.Answer_error()
-User_interfaces.start()
+            User_interfaces.answer_error()
+        #Для того что бы можно было выполнить матиматическое действие над результатом
+        User_interfaces.one_number = Calculation.result
+User_interfaces.start_user_interfaces()
